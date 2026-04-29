ef3cda1cea250f7647ecc3b3671e88bc0494b578
Varun Krishna <varunkrishnachess@gmail.com>
2026-04-29 22:05:53 +0530

    adding tavily search agent

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_tavily import TavilySearch
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.9)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
+
 def main():
     print("Hello from langchain-course!")
-
+    result = agent.invoke({"messages": HumanMessage(content="Search for 3 job openings for software engineers in India and summarize the results.")})
+    print(result)
 
 if __name__ == "__main__":
     main()
