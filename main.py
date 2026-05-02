39d9e6049d79fc53f18f5d338ac829d5fe64ab77
Varun Krishna <varunkrishnachess@gmail.com>
2026-05-02 09:35:16 +0530

    adding agent response

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from pydantic import BaseModel, Field
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,9 +10,19 @@ from langchain_core.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
+
+class Source(BaseModel):
+    """Schema for a source returned by the agent."""
+    url:str = Field(description="The URL of the source.")
+
+class AgentResponse(BaseModel):
+    """Schema for the agent's response."""
+    answer:str = Field(description="The answer to the user's query.")
+    sources: List[Source] = Field(default_factory=list, description="A list of sources used to generate the answer.")
+
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0.9)
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
     print("Hello from langchain-course!")
