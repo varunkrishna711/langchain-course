7a21d439ad784f32ca820afbecb06fec8e2f0c00
Varun Krishna <varunkrishnachess@gmail.com>
2026-05-13 20:12:25 +0530

    adding rag embeddings

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,29 @@
 from dotenv import load_dotenv
 import os
 
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 load_dotenv()
 
 
 if __name__ == "__main__":
     print("Ingesting....")
-    print(os.getenv("PINECONE_API_KEY"))
+    
+    loader = TextLoader("/home/varun/Documents/medium-blog.txt")
+    document = loader.load()
+
+    print("splitting")
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"Created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("Ingesting")
+
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
+    print("Finish")
